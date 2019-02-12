80338050d63f371e8c819466d8ff572fef7c00c8
GnaneswaraSarma <45159842+GnaneswaraSarma@users.noreply.github.com>
2019-02-13 00:30:52 +0530

    Add files via upload

---
+++ HelloWorld/src/Hello/Hello.java
@@ -0,0 +1,15 @@
+package Hello;
+
+public class Hello{
+	public static void main(String [] args) {
+		
+		System.out.println("My Name is Gnaneswara Sarma");
+		int mynumber = 10+ 5 + 2 * 10;
+		int mysecond = 30;
+		int mythird = 50;
+		int total = mynumber + mysecond + mythird;
+		
+		System.out.println("total = " + total);
+		
+}
+}
